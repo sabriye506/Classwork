189ff2f8c4b4a79b64a9050bf353be9f0b4ec408
Sabriyes Project <student.sbr.mohamed7@horizoncolumbus.org>
2021-01-31 16:23:21 -0500

    Initial commit

---
+++ src/horizoncolumbus/org/Main.java
@@ -0,0 +1,27 @@
+package horizoncolumbus.org;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	int [] numbers = new int [5];
+
+	for(int i = 0; i < 5; i++){
+	    numbers[i] = i + 1;
+    }
+
+	int sum = 0;
+
+	for(int i = 0; i < 5;i++){
+	    sum = sum + numbers[i];
+    }
+	for(int i = 0; i < 5;i++) {
+	    if(i==4){
+            System.out.println(numbers[i] + " = " + sum);
+        }
+	    else {
+            System.out.print(numbers[i] + " + ");
+        }
+    }
+    }
+}
